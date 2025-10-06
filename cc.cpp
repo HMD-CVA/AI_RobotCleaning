d2520dc02dfb088e19ce27da56d768f6020b4a32
HMD-CVA <huynhmyduc2005@gmail.com>
2025-10-06 11:26:36 +0700

    New  Commit

--- cc.cpp
+++ cc.cpp
@@ -1,223 +1,173 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-int n, H[1000], G[10001], F[10001], pre[10001], vis[10001], s, e;
-vector<vector<pair<int,int>>> edge; 
-
-signed main()
-{
-    freopen("input.txt", "r", stdin);
-    cin >> n;
-
-    s = 0; e = 7;
-
-    edge.resize(n);
-    for (int i=0;i<n;i++) {
-        for (int j=0;j<n;j++) {
-            int w; cin >> w; 
-            if (w>0) edge[i].push_back({j, w});
+class RobotCleaning{
+    private:
+        int n;
+        vector<vector<pair<int, int>>> edge;
+        vector<int> H;
+    public:
+        RobotCleaning(int _n, vector<vector<pair<int, int>>> _edge, vector<int> _H){
+            n = _n;
+            edge = _edge;
+            H = _H;
+            cout << "Robot initialized with " << n << " nodes" << endl;
         }
-    }
-    for (int i=0;i<n;i++) {
-        vis[i] = -1;
-        G[i] = F[i] = INT_MAX;
-        pre[i] = -1;
-        cin >> H[i];
-    }
-
-    G[s] = 0;
-    F[s] = H[s];
-    vis[s] = 1; // O = 1, C = 0
-
-    while(true) {
-        int N = -1, res = INT_MAX;
-        for (int i=0;i<n;i++) { // Chon N thuoc O co F[N] min
-            if (vis[i] == 1 && res > F[i]) {
-                res = F[i];
-                N = i;
-            }
-        }
-
-        if (N == -1 || N == e) break;
-
-        vis[N] = 0;
-        for (pair<int, int> i : edge[N]) {
-            int Q = i.first;
-            int W = i.second;
-
-            if (vis[Q]==1) {
-                if (G[Q] > G[N] + W) {
-                    G[Q] = G[N] + W;
-                    F[Q] = G[Q] + H[Q];
-                    pre[Q] = N;
+        void displayEdgeInfo() {
+            cout << "\nEdge Information:" << endl;
+            for (int i = 0; i < n; i++) {
+                if (!edge[i].empty()) {
+                    cout << "Node " << i << " -> ";
+                    for (const auto& neighbor : edge[i]) {
+                        cout << neighbor.first << "(cost:" << neighbor.second << ") ";
+                    }
+                    cout << endl;
                 }
             }
-            else if (vis[Q]==-1) {
-                G[Q] = G[N] + W;
-                F[Q] = G[Q] + H[Q];
-                pre[Q] = N;
-                vis[Q] = 1;
+        }
+        pair<int , vector<int>> findPath(int startId, int goalId) {
+            // cout << "Finding path: " << startId << " -> " << goalId << endl;
+            if (startId < 0 || startId >= n || goalId < 0 || goalId >= n) {
+                cout << "Invalid start or goal ID!" << endl;
+                return {};
+            }
+            vector<int> G(n, INT_MAX);
+            vector<int> F(n, INT_MAX);
+            vector<int> pre(n, -1);
+            vector<int> vis(n, -1);
+            // vector<int> H(n, 0);
+            // int goalX = goalId % (int)sqrt(n);
+            // int goalY = goalId / (int)sqrt(n);
+            // for (int i = 0; i < n; i++) {
+            //     int x = i % (int)sqrt(n);
+            //     int y = i / (int)sqrt(n);
+            //     H[i] = abs(x - goalX) + abs(y - goalY);
+            // }
+            G[startId] = 0;
+            F[startId] = H[startId];
+            vis[startId] = 1;
+            while (true) {
+                int current = -1;
+                int minF = INT_MAX;
+                
+                // Tìm node có F nhỏ nhất
+                for (int i = 0; i < n; i++) {
+                    if (vis[i] == 1 && F[i] < minF) {
+                        minF = F[i];
+                        current = i;
+                    }
+                }
+                
+                if (current == -1) {
+                    cout << "No path found!" << endl;
+                    return {};
+                }
+                
+                if (current == goalId) {
+                    // cout << "Path found! Cost: " << G[goalId] << endl;
+                    break;
+                }
+                
+                vis[current] = 0;
+                
+                // Duyệt neighbors
+                for (pair<int, int> neighbor : edge[current]) {
+                    int next = neighbor.first;
+                    int cost = neighbor.second;
+                    
+                    if (vis[next] == 1) {
+                        if (G[next] > G[current] + cost) {
+                            G[next] = G[current] + cost;
+                            F[next] = G[next] + H[next];
+                            pre[next] = current;
+                        }
+                    } else if (vis[next] == -1) {
+                        G[next] = G[current] + cost;
+                        F[next] = G[next] + H[next];
+                        pre[next] = current;
+                        vis[next] = 1;
+                    }
+                }
+            }
+            vector<int> path;
+            int node = goalId;
+            while (node != -1) {
+                path.push_back(node);
+                node = pre[node];
+            }
+            reverse(path.begin(), path.end());
+            
+            // cout << "Path: ";
+            // for (int i = 0; i < path.size(); i++) {
+            //     cout << path[i];
+            //     if (i != path.size() - 1) cout << " -> ";
+            // }
+            // cout << endl;
+            
+            return {G[goalId], path};
+        }
+        void cleanAllDirty(vector<int> dirtyNodes, int startId) {
+            if (dirtyNodes.empty()) {
+                cout << "No dirty nodes!" << endl;
+                return;
+            }
+            int current = startId;
+            vector<int> remainingDirt = dirtyNodes;
+            int totalCost = 0;
+            
+            while (!remainingDirt.empty()) {
+                cout << current << " ";
+                int minCost = INT_MAX, minNode = -1;
+                vector<int> minPath;
+                for (int i=0;i<remainingDirt.size();i++) {
+                    pair<int, vector<int>> res = findPath(current, remainingDirt[i]);
+                    if (minCost > res.first) {
+                        minCost = res.first;
+                        minNode = remainingDirt[i];
+                        minPath = res.second;
+                    }
+                }
+                if (minNode == -1) return ;
+
+                cout << "Move to node " << minNode << " (Cost: " << minCost << ")" << endl;
+                cout << "Path: ";
+                for (int node : minPath) cout << node << " ";
+                    cout << endl;
+                
+                totalCost += minCost;
+                current = minNode;
+                remainingDirt.erase(
+                    remove(remainingDirt.begin(), remainingDirt.end(), minNode),
+                    remainingDirt.end()
+                );
+                cout << "Cleaned node " << minNode << ". Remaining: " << remainingDirt.size() << " nodes" << endl;
             }
         }
+};
+
+signed main() 
+{
+    freopen("input.txt", "r", stdin);
+    int n; cin >> n;
+    vector<vector<pair<int, int>>> edge;
+    vector<int> H;
+    edge.resize(n);
+    H.resize(n);
+    for (int i=0;i<n;i++) {
+        for (int j=0;j<n;j++) {
+            int w;
+            cin >> w;
+            if (w > 0) edge[i].push_back({j, w});
+        }
     }
-    cout << G[e] << "\n";
+    for (int i=0;i<n;i++) cin >> H[i];
+    RobotCleaning rb(n, edge, H);
+    // rb.findPath(0, 7);
 
-    int tmp = e;
-    do {
-        cout << tmp << " ";
-        tmp = pre[tmp];
-    } while(pre[tmp]!=-1);
-    cout << s;
-}
+    // vector<int> sP = rb.findPath(0, 7).second;
+    // for (int v:sP) cout << v << " "; 
+    // cout << endl ;
+    // cout << rb.findPath(0, 7).first; 
 
-
-
-
-// // Định nghĩa 1 ô trong ma trận đường đi
-// class Cells{
-//     private:
-//         int x, y, cost;         // Toạ độ
-//         bool isObstactle; // Chướng ngại vật
-//         bool hasDirt;     // Chỗ cần lau
-//     public:
-//         int getOx() {
-//             return x;
-//         }
-//         int getOy() {
-//             return y;
-//         }
-//         int getCost() {
-//             return cost;
-//         }
-//         bool getObstactle() {
-//             return isObstactle;
-//         }
-//         bool getDirty() {
-//             return hasDirt;
-//         }
-//         void setToaDoCost(int _x, int _y, int _cost) {
-//             x = _x;
-//             y = _y;
-//             cost = _cost;
-//             return;
-//         }
-//         void setObstactle(bool _obsactle) {
-//             isObstactle = _obsactle;
-//             return;
-//         }
-//         void setDirty(bool _dirt) {
-//             hasDirt = _dirt;
-//             return;
-//         }
-//         Cells(int _x = 0, int _y = 0, int _cost = 0, bool _obstactle = false, bool _dirt = false) {
-//             x = _x;
-//             y = _y;
-//             cost = _cost;
-//             isObstactle = _obstactle;
-//             hasDirt = _dirt;
-//         } 
-//         // bool Cells:: operator == (Cells &other) {
-//         //     return this->x == other.x && this->y == other.y;
-//         // } 
-// };
-
-// class CleaningRobot{
-//     private:
-//         vector<vector<Cells>> grid; // Lưới toạ độ
-//         int width, height;
-//         Cells startCell;
-//         vector<Cells> dirtyCells;
-//     public:
-//         void setWH (int _we, int _he) {
-//             width = _we;
-//             height = _he;
-//             return;
-//         }
-//         bool isValid(int x, int y) {
-//             return x >= 0 && x < width && y >= 0 && y < height;
-//         }
-//         CleaningRobot(int _width, int _height) { // Khởi tạo ma trận
-//             width = _width;
-//             height = _height;
-//             grid.resize(height, vector<Cells>(width));
-//             for (int i=0;i<width;i++)
-//                 for (int j=0;j<height;j++)
-//                     grid[i][j] = Cells(i, j, 0, false, false);
-//         }
-//         void setStart(int x, int y) {
-//             if (isValid(x, y)) startCell = grid[x][y];
-//         }
-//         void setCost(int x, int y, int cost) {
-//             if (!isValid(x, y)) return;
-//             grid[x][y].setToaDoCost(x, y, cost);
-//         }
-//         void setObstactle(int x, int y) {
-//             if (isValid(x, y)) grid[x][y].setObstactle(true);
-//         }
-//         void setDirt(int x, int y) {
-//             if (isValid(x, y)) {
-//                 grid[y][x].setDirty(true);
-//                 dirtyCells.push_back(grid[y][x]);
-//             }
-//         }
-//         double heuristic(Cells a, Cells b) { // h(n): Ước lượng khoảng cách từ n đến điểm đích
-//             return abs(a.getOx() - b.getOx()) + abs(a.getOy() - b.getOy());
-//         }
-//         vector<Cells> findPath(Cells start, Cells goal) {
-//             if (!isValid(start.getOx(), start.getOy()) || !isValid(goal.getOx(), goal.getOy())) return {};
-//             vector<int> Close, Open, gS(width, INT_MAX), hS, fS(width, INT_MAX);
-//             int startE = start.getOx();
-//             int endE = goal.getOx();
-//             Open.push_back(startE);
-//             gS[startE] = 0;
-//             fS[startE] = hS[startE];
-//             while (!Open.empty()) {
-//                 int fN = INT_MAX, N = -1;
-//                 for (int i:Open) { // Chọn N thuộc O có f(N) nhỏ nhất
-//                     if (fN > fS[i]) {
-//                         fN = fS[i];
-//                         N = i;
-//                     }
-//                 }
-//                 if (N==endE) {
-//                     break;
-//                 }
-//                 Open.erase(remove(Open.begin(), Open.end(), N), Open.end());
-//                 Close.push_back(N);
-//             }
-//         }
-//         void displayGrid() {
-//             cout << "\nBản đồ:" << endl;
-//             for (int y = 0; y < height; y++) {
-//                 for (int x = 0; x < width; x++) {
-//                     // if (grid[y][x].getOx() == startCell.getOx() && grid[y][x].getOy() == startCell.getOy()) {
-//                     //     cout << "S "; // Start
-//                     // } else if (grid[y][x].getObstactle()) {
-//                     //     cout << "X "; // Vật cản
-//                     // } else if (grid[y][x].getDirty()) {
-//                     //     cout << "D "; // Vết bẩn
-//                     // } else {
-//                     //    cout << ". "; // Ô trống
-//                     //}
-//                     cout << grid[x][y].getCost() << " " ;
-//                 }
-//                 cout << endl;
-//             }
-//         }
-// };
-
-// signed main()
-// {
-//     freopen("input.txt", "r", stdin);
-//     int n; cin >> n;
-//     int h[n];
-//     CleaningRobot rb(n, n);
-//     for (int i=0;i<n;i++) {
-//         for (int j=0;j<n;j++) {
-//             int _cost;
-//             cin >> _cost;
-//             rb.setCost(i, j, _cost);
-//         }
-//     }
-//     rb.displayGrid();
-// }
+    vector<int> dirtyNodes = {2, 5, 7};
+    rb.cleanAllDirty(dirtyNodes, 0);
+}   
